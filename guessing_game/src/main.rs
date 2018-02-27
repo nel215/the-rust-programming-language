efe87d620e3b67ab6a9e2224057e7eb2695cf5bf
nel215 <otomo.yuhei@gmail.com>
2018-02-27 22:53:34 +0900

    Apply rustfmt

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -15,8 +15,9 @@ fn main() {
 
         let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
+        io::stdin().read_line(&mut guess).expect(
+            "Failed to read line",
+        );
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
@@ -26,9 +27,9 @@ fn main() {
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less    => println!("Too small"),
+            Ordering::Less => println!("Too small"),
             Ordering::Greater => println!("Too big"),
-            Ordering::Equal   => {
+            Ordering::Equal => {
                 println!("You win");
                 break;
             }
